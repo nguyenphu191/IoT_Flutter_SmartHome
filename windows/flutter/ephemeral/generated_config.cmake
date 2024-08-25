d338f03de39fad5b5823a2869a5db031c53c2381
nguyenphu191 <149788592+nguyenphu191@users.noreply.github.com>
2024-08-25 22:38:15 +0700

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\DOCUMENT\\FLUTER\\flutter_shopping_app - Sao chép" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=C:\\DOCUMENT\\FLUTER\\flutter_shopping_app - Sao chép"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\DOCUMENT\\FLUTER\\flutter_shopping_app - Sao chép\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\DOCUMENT\\FLUTER\\flutter_shopping_app - Sao chép"
+  "FLUTTER_TARGET=C:\\DOCUMENT\\FLUTER\\flutter_shopping_app - Sao chép\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9jNGNkNDhlMTg2NDYwYjMyZDQ0NTg1Y2UzYzEwMzI3MWFiNjc2MzU1Lw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\DOCUMENT\\FLUTER\\flutter_shopping_app - Sao chép\\.dart_tool\\package_config.json"
+)
